f0865cdf4abbe79713ff3853602f0df1ee96c218
Diego Cândido <diegocndd4@gmail.com>
2024-10-09 09:32:40 -0300

    afgfds

--- test_env.py
+++ test_env.py
@@ -2,18 +2,12 @@ import os
 
 def main():
     # Substitua 'MY_SECRET' pelo nome da sua variável de ambiente
-    secret_value = os.getenv('FIRST_ENV')
-    secret_value2 = os.getenv('NEXT_HI_EVERYONE')
-    
-    if secret_value:
-        print(f"O valor da variável MY_SECRET é: {secret_value}")
+    SECRET1 = os.getenv('SECRET1')
+    print(SECRET1)
+    if SECRET1:
+        print(f"O valor da variável MY_SECRET é: {SECRET1}")
     else:
         print("A variável MY_SECRET não está definida.")
-
-    if secret_value2:
-        print(f"O valor da variável MY_SECRET2 é: {secret_value2}")
-    else:
-        print("A variável MY_SECRET2 não está definida.")
 
 if __name__ == "__main__":
     main()
